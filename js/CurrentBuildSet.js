04c77e2aa5a413957f43e144a40b78edef9a83b8
Christian Danielsen <ckdanielsen@gmail.com>
2015-01-30 17:00:34 -0800

    add getDevBuildObjects method

--- js/CurrentBuildSet.js
+++ js/CurrentBuildSet.js
@@ -2,18 +2,29 @@
 var CurrentBuildSet = {
 	initialize : function(baseUrl, resourcePath) {
 		this.builds = [];
-		this.status = "";
+		this.buildNames = [];
+    this.status = "";
     this.baseUrl = baseUrl;
     this.resourcePath = resourcePath;
 	},
 	getDevBuildNames : function() {
     var that = this;
-    var superNewRequest = Object.create(AsyncGetRequest);
-    superNewRequest.initialize(this.baseUrl, this.resourcePath);
-    superNewRequest.execute();
-    var currentDevInfo = superNewRequest.response;
+    var newRequest = Object.create(AsyncGetRequest);
+    newRequest.initialize(this.baseUrl, this.resourcePath);
+    newRequest.execute();
+    var currentDevInfo = newRequest.response;
     currentDevInfo.buildTypes.buildType.forEach(function(build) {
-      that.builds.push(build.id);
+      that.buildNames.push(build.id);
     });
-	}
+	},
+  getDevBuildObjects : function() {
+    that = this;
+    var baseUrl = 'http://teamcity:8080/guestAuth/app/rest/builds/buildType:'
+    this.buildNames.forEach(function(buildName) {
+      var newRequest = Object.create(AsyncGetRequest);
+      newRequest.initialize(baseUrl, buildName);
+      newRequest.execute();
+      that.builds.push(newRequest.response)
+    });    
+  }
 }
--- spec/CurrentBuildSetSpec.js
+++ spec/CurrentBuildSetSpec.js
@@ -2,10 +2,11 @@
 describe("CurrentBuildSet", function() {
 	
   describe("#initialize", function() {
-  	it("should set builds property to be an empty array", function() {
+  	it("should set build and build names properties to be a empty arrays", function() {
       var testBuildSet = Object.create(CurrentBuildSet);
       testBuildSet.initialize();
   		expect(testBuildSet.builds).toEqual([]);
+      expect(testBuildSet.buildNames).toEqual([]);
   	});
 
   	it("should set status to be an empty string", function() {
@@ -25,14 +26,25 @@ describe("CurrentBuildSet", function() {
   });  //end #initialize
 
   describe("#getDevBuildNames", function(){
-    it("should populate the builds array with the current build names", function(){
+    it("should populate the build names array with the current build names", function(){
       var baseUrl = "http://teamcity:8080/guestAuth/app/rest/";
       var resourcePath = "projects/Dev";
       var testBuildSet = Object.create(CurrentBuildSet);
       testBuildSet.initialize(baseUrl, resourcePath);
       testBuildSet.getDevBuildNames();
-      expect(testBuildSet.builds).toBeNonEmptyArray();
+      expect(testBuildSet.buildNames).toBeNonEmptyArray();
     });
 	}); //end #getDevBuildNames
 
+  describe("#getDevBuildObjects", function() {
+    it("should populate the builds array with the current build objects", function(){
+      var baseUrl = "http://teamcity:8080/guestAuth/app/rest/";
+      var resourcePath = "projects/Dev";
+      var testBuildSet = Object.create(CurrentBuildSet);
+      testBuildSet.initialize(baseUrl, resourcePath);
+      testBuildSet.getDevBuildNames();
+      testBuildSet.getDevBuildObjects();
+      expect(testBuildSet.builds).toBeNonEmptyArray();
+    });
+  });
 });
